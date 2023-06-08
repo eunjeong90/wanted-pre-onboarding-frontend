eed6a0b62f866b31464f67df53e3d20716780aba
JangEunJeong <cuttlefish77777@gmail.com>
2023-06-08 15:39:52 +0900

    feat: CRA custom init + eslint, prettier setting

---
+++ .eslintrc.js
@@ -0,0 +1,54 @@
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint", "prettier"],
+  extends: [
+    "plugin:react/recommended",
+    "plugin:jsx-a11y/recommended",
+    "plugin:import/errors",
+    "plugin:import/warnings",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:prettier/recommended",
+    "prettier",
+    "plugin:import/typescript",
+  ],
+  rules: {
+    "prettier/prettier": [
+      "warn",
+      {
+        endOfLine: "auto",
+        printWidth: 80,
+        trailingComma: "es5",
+        semi: true,
+        singleQuote: false,
+        jsxSingleQuote: false,
+        useTabs: false,
+        tabWidth: 2,
+      },
+    ],
+    "comma-dangle": "off",
+    "@typescript-eslint/comma-dangle": ["error", "only-multiline"],
+    "linebreak-style": 0,
+    "import/prefer-default-export": 0,
+    "import/extensions": 0,
+    "no-use-before-define": 0,
+    "import/no-unresolved": 0,
+    "react/react-in-jsx-scope": 0,
+    "import/no-extraneous-dependencies": 0,
+    "no-shadow": 0,
+    "react/prop-types": 0,
+    "react/jsx-filename-extension": [
+      2,
+      { extensions: [".js", ".jsx", ".ts", ".tsx"] },
+    ],
+    "jsx-a11y/no-noninteractive-element-interactions": 0,
+    "@typescript-eslint/explicit-module-boundary-types": 0,
+  },
+  settings: {
+    "import/resolver": {
+      node: {
+        extensions: [".js", ".jsx", ".ts", ".tsx"],
+      },
+    },
+  },
+};
